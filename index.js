87447c2f6ff12b097174444cd962983aa193f928
AsacKami <77325112+AsacKami@users.noreply.github.com>
2021-02-27 16:29:44 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var a = document.getElementsByClassName("usr");
+var b =document.getElementsByClassName("pass");
+if (a==b){
+  document.getElementsByid("para").innerHTML="CONGS";
+}
+else{
+  document.getElementsByid("para").innerHTML="apopap";
+}
